ff3bcf3346762647ac2a578e1c43be20aa2d8a3b
Leonardo López Romero <leo_fan129albert@hotmail.com>
2018-10-09 15:55:27 -0500

    Initial Commit

---
+++ squashApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  squashApp
+//
+//  Created by Leo on 10/9/18.
+//  Copyright © 2018 Leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
